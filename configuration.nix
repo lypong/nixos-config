c53ce37f44e48f635af8cf902b92dd7c59d9a130
seb <sfv.jacob@gmail.com>
2023-03-29 17:19:40 +0200

    Using Pipewire

--- configuration.nix
+++ configuration.nix
@@ -58,8 +58,14 @@
   # services.printing.enable = true;
 
   # Enable sound.
-  # sound.enable = true;
-  # hardware.pulseaudio.enable = true;
+  services.pipewire = {
+  enable = true;
+  alsa.enable = true;
+  alsa.support32Bit = true;
+  pulse.enable = true;
+  # If you want to use JACK applications, uncomment this
+  #jack.enable = true;
+  };
 
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
