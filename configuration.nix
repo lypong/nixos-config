769b2acd2fb6b2fdc7f4bffc1d5a021e8994f1f6
seb <sfv.jacob@gmail.com>
2023-04-11 14:24:25 +0200

    Add xdg-utils as a system package

--- configuration.nix
+++ configuration.nix
@@ -91,6 +91,7 @@
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
     git
+    xdg-utils
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
